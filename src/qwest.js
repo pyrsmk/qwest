ea0e5db77612f76c1938cc1a2c3abf4ab091bfe5
Branko Vukelic <studio@brankovukelic.com>
2014-01-27 18:18:18 +0100

    Fixes #5: Headers mangled by missing dash
    This commit fixes the issue discussed in ticket #5. The code that removed the - has been removed and the headers are now added intact.

--- src/qwest.js
+++ src/qwest.js
@@ -277,11 +277,6 @@
         }
         // Prepare headers
         for(i in headers){
-            // Format
-            j=i.replace(/(^|-)(\w)/g,toUpper);
-            headers[j]=headers[i];
-            delete headers[i];
-            // Set header
             xhr.setRequestHeader(i,headers[i]);
         }
         if(!headers['Content-Type'] && serialized && method=='POST'){
