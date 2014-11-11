04f5d3bd311db24b2b89728aab7364715d956f32
pyrsmk <dev@dreamysource.fr>
2014-11-11 15:34:51 +0100

    Update build system

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,28 @@
 module.exports = function(grunt) {
 
+	// Requires
 	require('jit-grunt')(grunt);
+	var fs=require('fs'),
+		child_process=require('child_process');
+
+	// Get library name
+	var excludes=['.','..','ender.js','footer.js'],
+		files=fs.readdirSync('src'),
+		name,i,j;
+	if(files.indexOf('header.js')!==-1){
+		name='header.js';
+	}
+	else{
+		for(i=0,j=files.length;i<j;++i){
+			if(excludes.indexOf(files[i])==-1){
+				name=files[i];
+				break;
+			}
+		}
+	}
 
 	// Get version
-	var child_process=require('child_process'),
-		fs=require('fs'),
-		version=fs.readFileSync('README.md',{encoding:'utf8'}).match(/^\w+ ([0-9.]+)/)[1];
+	var version=fs.readFileSync('src/'+name,{encoding:'utf8'}).match(/^\/\*\! \w+ ([0-9.]+)/)[1];
 
 	grunt.initConfig({
 		// Load bower file
@@ -50,9 +67,9 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		// Prepare package.json
+		// Change library version across files
 		'string-replace': {
-			dev_define: {
+			package_json: {
 				files: {'package.json':'package.json'},
 				options:{
 					replacements: [{
@@ -60,9 +77,18 @@ module.exports = function(grunt) {
 						replacement: '"version": "'+version+'",'
 					}]
 				}
+			},
+			README_md: {
+				files: {'README.md':'README.md'},
+				options:{
+					replacements: [{
+						pattern: /^(\w+) [0-9.]+/,
+						replacement: '$1 '+version
+					}]
+				}
 			}
 		},
-		// Publish on NPM
+		// Publish on package managers
 		shell: {
 			options: {
 				stderr: false
@@ -77,6 +103,6 @@ module.exports = function(grunt) {
 	});
 
 	// Define tasks
-	grunt.registerTask('default', ['clean', 'jshint', 'uglify', 'string-replace'/*, 'shell'*/]);
+	grunt.registerTask('default', ['clean', 'jshint', 'uglify', 'string-replace', 'shell']);
 
 };
