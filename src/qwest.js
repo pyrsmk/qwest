c9ff7e5fe54afa9271394175373d0086d6010498
singerxt <mat.spiewak@gmail.com>
2015-06-03 03:05:03 +0200

    remove test method

--- src/qwest.js
+++ src/qwest.js
@@ -17,29 +17,6 @@ class qwest {
 
   /**
    *
-   * test will be removed
-   */
-
-  test () {
-    let promise = new Promise(function(resolve, reject) {
-      let num = Math.random() * 15;
-      console.log(num);
-      if (num < 10) {
-        resolve("Stuff worked!");
-      } else {
-        reject(Error("It broke"));
-      }
-    });
-
-    promise.then(function (result) {
-      console.log(result);
-    }, function (err) {
-      console.log(err);
-    });
-  }
-
-  /**
-   *
    * @param url
    * @param options
    */
