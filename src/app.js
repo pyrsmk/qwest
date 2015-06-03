37ec1714fc77a5e4f57bc9efeec8b42fa1cf8d2c
singerxt <mat.spiewak@gmail.com>
2015-06-03 14:18:07 +0200

    updated readme

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import {qwest} from './qwest.js';
 
 let q = new qwest();
 
-q.httpGet('https://developer.mozilla.org/en-US/search.json?page=2').then(function (r) {
+q.$httpGet('https://developer.mozilla.org/en-US/search.json?page=2').then(function (r) {
   console.log('response', r);
 }).catch(function () {
   console.log('something went wrong');
