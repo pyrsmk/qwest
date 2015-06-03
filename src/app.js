1e20b261512f54af640510c4fc84056ddbb71d73
singerxt <mat.spiewak@gmail.com>
2015-06-03 02:54:16 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import {qwest} from './qwest.js';
 
 let q = new qwest();
-q.test();
+
+q.httpGet('https://developer.mozilla.org/en-US/search.json?page=2').then(function (r) {
+  console.log('response', r);
+}).catch(function () {
+  console.log('something went wrong');
+});
--- src/qwest.js
+++ src/qwest.js
@@ -43,12 +43,58 @@ class qwest {
    * @param url
    * @param options
    */
-  sendGet (url, options) {
-    let that = this;
-    return new Promise(function (resolve, reject) {
-      let xhr = that._Utils.getXHR();
 
+  createRequest (method, url, args) {
+    let that = this;
+    let promise = new Promise(function (resolve, reject) {
+      let client = that._Utils.getXHR();
+      let uri = url;
+
+      /*if(that._Utils.isXHR2()) {
+        console.warn('qwest dont have support for IE - yet');
+        return false;
+      }*/
+
+      if (args && (method === 'POST') || method === 'PUT') {
+        let argCount = 0;
+        uri += '?';
+
+        for(let key in args) {
+          if(args.hasOwnProperty(key) && argCount++) {
+            uri += '&';
+          }
+
+          /**
+           * refactor to template string ?
+           */
+          uri += encodeURIComponent(key) + '=' + encodeURIComponent(args[key]);
+        }
+      }
+
+      client.onload = function () {
+        if (this.status === 200) {
+          resolve(this.response);
+        } else {
+          reject(this.statusText);
+        }
+      };
+      client.onerror = function () {
+        reject(this.statusText);
+      };
+
+      client.open(method,uri);
+      client.send(null);
     });
+
+    return promise;
+  }
+
+  httpGet (url, args) {
+    if (url) {
+      return this.createRequest('GET', url, args);
+    } else {
+      throw 'qwest::missing url';
+    }
   }
 }
 
