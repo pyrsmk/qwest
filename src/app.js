d245446439901c7f9be879b36b774e5e9a475c52
singerxt <mat.spiewak@gmail.com>
2015-06-03 16:12:38 +0200

    Basic get,post,delete methods

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import {qwest} from './qwest.js';
 
 let q = new qwest();
 
-q.httpGet('https://developer.mozilla.org/en-US/search.json?page=2').then(function (r) {
-  console.log('response', r);
-}).catch(function () {
-  console.log('something went wrong');
+q.get('https://developer.mozilla.org/en-US/search.json?page=2').then(function (r) {
+  console.log('respone', r);
+}).then(function () {
+  console.log('Hello world');
 });
