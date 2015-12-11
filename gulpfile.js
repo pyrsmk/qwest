d01c965014e8828da96cf690c7bb34ddb2455ed0
pyrsmk <dev@dreamysource.fr>
2015-12-11 12:57:53 +0100

    Add non-minified version

--- gulpfile.js
+++ gulpfile.js
@@ -75,6 +75,7 @@ gulp.task('build', ['version', 'lint'], function() {
 		
 				}) )
 				.pipe( derequire() )
+				.pipe( gulp.dest('.') )
 				.pipe( uglify() )
 				.pipe( rename(name+'.min.js') )
 				.pipe( gulp.dest('.') )
